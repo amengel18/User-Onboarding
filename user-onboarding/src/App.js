195fba25784885c1050f6b0640a4cc7ea8c1c648
Angela Engel <amengel.ae@icloud.com>
2021-02-18 19:33:55 -0700

    Project continued

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,10 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import UserForm from './UserForm';
+import axios from 'axios';
+
+
+const initialFormValues = {
+  name: '',
+  email: '',
+  password: '',
+  terms: false
+}
+
+const initialFormErros = {
+  name: '',
+  email: '',
+  password: '',
+  terms: ''
+}
+
+const initialUsers = []
+const initialDisabled = true
 
 function App() {
+  const [users, setUsers] = useState(initialUsers)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErros, setFormErrors] = useState(initialFormErros)
+  const [disabled, setDisabled] = useState(initialDisabled)
+  
   return (
 
+  )
 }
 
 export default App;
